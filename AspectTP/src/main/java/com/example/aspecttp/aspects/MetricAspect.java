1c8c982ca9ee3d70c1c0459518aa2aba9f8a87f6
raixonomas <83031889+raixonomas@users.noreply.github.com>
2026-05-18 19:26:24 -0400

    asdg hygulgjhgdas dfyhytdj

--- AspectTP/src/main/java/com/example/aspecttp/aspects/MetricAspect.java
+++ AspectTP/src/main/java/com/example/aspecttp/aspects/MetricAspect.java
@@ -2,7 +2,13 @@ package com.example.aspecttp.aspects;
 
 import com.example.aspecttp.classes.context.TelemetryContext;
 import com.example.aspecttp.classes.events.MetricEvent;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 
+import java.util.Map;
+
+@Aspect
 public class MetricAspect {
 
     @Around("execution(* com.example..*(..))")
@@ -15,10 +21,12 @@ public class MetricAspect {
         long durationMs = (System.nanoTime() - start) / 1_000_000;
 
         MetricEvent event = new MetricEvent(
-                "execution_time_ms",
-                durationMs,
                 pjp.getSignature().getDeclaringTypeName(),
-                pjp.getSignature().getName()
+                pjp.getSignature().getName(),
+                Map.of(),
+                "execution_time_ms",
+                durationMs
+
         );
 
         TelemetryContext
--- AspectTP/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.aspecttp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.aspecttp to javafx.fxml;
-    exports com.example.aspecttp;
-}
